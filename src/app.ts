5f79bafe242b93b65c4d04e6005a8718bc42cd8c
Swagata Prateek <thehoneymad@hotmail.com>
2017-03-28 03:26:30 +0200

    very simple token mechanism in place

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import * as path from 'path';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
 
 import { WebhookController } from './controllers/webhookController';
 // Creates and configures an ExpressJS web server.
 class App {
-
+  public auth_token: string;
   // ref to Express instance
   public express: express.Application;
   public webhookController: WebhookController;
@@ -17,6 +18,16 @@ class App {
     this.webhookController = new WebhookController();
     this.middleware();
     this.routes();
+    this.init();
+  }
+
+  authTokenChecker = function (req, res, nextFunc) {
+    if (this.auth_token == req.query.token) {
+      nextFunc();
+    }
+    else {
+      res.status(401).send('Unauthorized to send webhook requests');
+    }
   }
 
   // Configure Express middleware.
@@ -24,6 +35,7 @@ class App {
     this.express.use(logger('dev'));
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use(this.authTokenChecker.bind(this));
   }
 
   // Configure API endpoints.
@@ -34,12 +46,22 @@ class App {
     let router = express.Router();
     // placeholder route handler
     router.get('/', (req, res, next) => {
+      req.query
       res.json({
         message: 'Welcome to NerdCats docker pull webhook host'
       });
     });
     this.express.use('/', router);
     this.express.use('/pull', this.webhookController.router);
+  }
+
+  init() {
+    let config = JSON.parse(fs.readFileSync('dist/config.json', 'utf8'));
+    if (!config || !config.auth_token) {
+      console.error('no configuration or no auth_token in configuration');
+      process.exit();
+    }
+    this.auth_token = config.auth_token;
   }
 
 }
