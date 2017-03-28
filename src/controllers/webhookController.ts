26e119028363346f7b85625ee2c82c333f162945
Swagata Prateek <thehoneymad@hotmail.com>
2017-03-28 02:59:47 +0200

    works with the post controller now

--- src/controllers/webhookController.ts
+++ src/controllers/webhookController.ts
@@ -18,31 +18,45 @@ export class WebhookController {
      * @param next Next handler in middleware (Don't use if you don't know what you are doing)
      */
     public pull(request: Request, response: Response, nextFunc: NextFunction) {
-        let proc = child_process.spawn('sh', ['dist/commands/docker-pull.sh', 'nginx:latest', '-p 5000:80']);
+        // Lets try to determine what are we supposed to do here
+        let body = request.body;
+        // the default mode is of course docker. The others are not supported yet
+        body.mode = body.mode || 'docker';
+        if (body.mode == 'docker') {
+            if (!body.image) { return response.status(400).send('no docker image name provided'); }
+            body.arguments = body.arguments || '';
 
-        let resStream: string = "";
+            let proc = child_process.spawn('sh', ['dist/commands/docker-pull.sh', body.image, body.arguments]);
 
-        proc.stdout.addListener('data', (data) => {
-            resStream += data.toString().trim();
-            var lines = resStream.split("\n");
-            for (var i in lines) {
-                if (i.trim().length) {
-                    response.write('stdout: ' + lines[i] + "\n");
-                    console.log('data: ' + lines[i]);
+            let resStream: string = "";
+
+            proc.stdout.addListener('data', (data) => {
+                resStream += data.toString().trim();
+                var lines = resStream.split("\n");
+                for (var i in lines) {
+                    if (i.trim().length) {
+                        response.write('stdout: ' + lines[i] + "\n");
+                        console.log('data: ' + lines[i]);
+                    }
                 }
-            }
-        });
+            });
 
-        proc.stderr.addListener('data', (data) => {
-            response.status(500);
-            response.write('ERR:' + data);
-            console.log('ERR:' + data);
-        });
+            proc.stderr.addListener('data', (data) => {
+                response.status(500);
+                response.write('ERR:' + data);
+                console.log('ERR:' + data);
+            });
 
-        proc.addListener('exit', (code) => {
-            response.end('child process exited with code ' + code);
-            console.log('child process exited with code ' + code);
-        });
+            proc.addListener('exit', (code) => {
+                response.end('child process exited with code ' + code);
+                console.log('child process exited with code ' + code);
+            });
+        }
+        else {
+            // Sending back Not Implemented for now since we dont really know what we will do here
+            response.sendStatus(501);
+            return;
+        }
     }
 
     init() {
